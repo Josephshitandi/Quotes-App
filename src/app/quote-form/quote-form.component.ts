4499d2cfaae13933f88ee49a6c05b42c06ca9a5d
Josephshitandi <jsphshtnd@gmail.com>
2020-09-26 17:13:51 +0300

    Add new quote form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Quote} from '../quote'
 
 @Component({
@@ -7,10 +7,16 @@ import { Quote} from '../quote'
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  newGoal = new Quote(0,"","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+this.addQuote.emit(this.newGoal);
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
