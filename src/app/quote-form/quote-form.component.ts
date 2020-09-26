b6d0356493ea1260930854cbf7c5f9a308b7697e
Josephshitandi <jsphshtnd@gmail.com>
2020-09-26 18:16:39 +0300

    Import form module to root module

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -8,12 +8,12 @@ import { Quote} from '../quote'
 })
 export class QuoteFormComponent implements OnInit {
 
-//   newQuote = new Quote(0,"","",new Date());
-//   @Output() addQuote = new EventEmitter<Quote>();
+  newQuote = new Quote(0,"","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
 
-//   submitQuote(){
-// this.addQuote.emit(this.newQuote);
-//   }
+  submitQuote(){
+this.addQuote.emit(this.newQuote);
+  }
 
   constructor() { }
 
