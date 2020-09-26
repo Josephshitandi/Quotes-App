63e0a3ecdea2086272b996ff7977c5497c76e8fa
Josephshitandi <jsphshtnd@gmail.com>
2020-09-26 14:57:07 +0300

    Add a constructor in class Quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,18 @@
 export class Quote {
+    id: number;
+    text: string;
+    author: string;
+    date: Date;
+    upvote: number;
+    downvote: number;
+
+    constructor(id: number,text:string, author:string, ){
+        this.id = id;
+        this.text = text;
+        this.author = author;
+        this.date = new Date();
+        this.upvote = 0;
+        this.downvote = 0;
+    }
+
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from  '../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  // quotes: Quote[] =[
+  //   new Quote(1,'Better your best','Sophie'),
+  //   new Quote(2,'Better your best','Sophie'),
+  //   new Quote(3,'Better your best','Sophie'),
+  //   new Quote(4,'Better your best','Sophie'),
+  // ];
+  // upVote(index){
+  //   this.quotes[index].upvote++;
+  // }
+
+  // downVote(index){
+  //   this.quotes[index].downvote++;
+  // }
 
   constructor() { }
 
