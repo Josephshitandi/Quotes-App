fa2e92e3216032c9289437f11b36ac395589cb74
Josephshitandi <jsphshtnd@gmail.com>
2020-09-27 14:58:22 +0300

    Add a directive that shows the quote with highest upvote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
     new Quote(1, 'Better your best','Peter', 'Joseph',new Date(2019,6,9)),
-    new Quote(2, 'If people are doubting how far you can go, go so far that you can’t hear them anymore.', ' Michele Ruiz','Joseph',new Date(2019,6,9)), 
-    new Quote(3, 'We need to accept that we won’t always make the right decisions, that we’ll screw up royally sometimes – understanding that failure is not the opposite of success, it’s part of success.','Arianna Huffington','Joseph',new Date(2019,6,9)),
-    new Quote(4, 'Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.','Joss Whedon','Joseph',new Date(2019,6,9)),
-    new Quote(5, 'I wake up every morning and think to myself, ‘how far can I push this company in the next 24 hours.','Leah Busque','Joseph',new Date(2019,6,9)),
+    new Quote(2, 'If people are doubting how far you can go, go so far that you can’t hear them anymore.', ' Michele Ruiz','Joseph',new Date(2019,8,9)), 
+    new Quote(3, 'We need to accept that we won’t always make the right decisions, that we’ll screw up royally sometimes – understanding that failure is not the opposite of success, it’s part of success.','Arianna Huffington','Joseph',new Date(2020,6,9)),
+    new Quote(4, 'Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.','Joss Whedon','Joseph',new Date(2019,6,7)),
+    new Quote(5, 'I wake up every morning and think to myself, ‘how far can I push this company in the next 24 hours.','Leah Busque','Joseph',new Date(2019,10,9)),
   ];
   upVote(index) {
 
@@ -42,7 +42,19 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
-  
+  highestUpVote() {
+    let highestUpVoteQuote = new Quote(0, '', '', '', new Date());
+    for (let i = 0; i < this.quotes.length; i++) {
+      if (this.quotes[i].upvote > highestUpVoteQuote.upvote) {
+        highestUpVoteQuote = this.quotes[i];
+      }
+    }
+    if (highestUpVoteQuote.upvote > 0) {
+      return highestUpVoteQuote;
+    } else {
+      return;
+    }
+  }
 
 
   constructor() { }
